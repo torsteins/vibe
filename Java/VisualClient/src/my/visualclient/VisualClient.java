646a89d8c45bb127c61aa2fc92ba755cd9bb48a8
torsteins <torsteins@mi061170.lan>
2013-02-07 19:49:09 -0500

    Some dumb change

--- Java/VisualClient/src/my/visualclient/VisualClient.java
+++ Java/VisualClient/src/my/visualclient/VisualClient.java
@@ -15,5 +15,6 @@ public class VisualClient {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Added something to code");
     }
 }
