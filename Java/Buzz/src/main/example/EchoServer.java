2775c64fd184abb0d9d7cab2ece01b7cd5048db8
torsteins <torsteins@mi061170.lan>
2013-02-20 01:19:58 -0500

    First integration of Vibe in server

--- Java/Buzz/src/main/example/EchoServer.java
+++ Java/Buzz/src/main/example/EchoServer.java
@@ -1,3 +1,5 @@
+import gnu.io.CommPortIdentifier;
+import gnu.io.PortInUseException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,11 +11,16 @@ import org.java_websocket.WebSocket;
 import org.java_websocket.WebSocketImpl;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
+import test.TestVibe;
+import vibe.Vibe;
 
 /**
  * Simple EchoServer using Java-WebSocket API
  */
 public class EchoServer extends WebSocketServer {
+    private static final String SERIALPORTNAME = "/dev/tty.usbserial-AD01UBSI";
+    private static Vibe vibe;
+    private static boolean on = false;
 
 	public EchoServer( int port ) throws UnknownHostException {
 		super( new InetSocketAddress( port ) );
@@ -35,10 +42,32 @@ public class EchoServer extends WebSocketServer {
 
 	@Override
 	public void onMessage( WebSocket conn, String message ) {
+        
+        if (EchoServer.on) {
+            EchoServer.vibe.setState(0, 0, 0);
+        }
+        else {
+            EchoServer.vibe.setState(0, 0, Integer.MAX_VALUE);
+        }
+        
+        EchoServer.vibe.sendUpate();
+        EchoServer.on = ! EchoServer.on;
+        String rspv = EchoServer.vibe.getMessage();
+        
 		System.out.println( conn + ": " + message );
+        System.out.println( "Arduino response: " + rspv);
+        
 	}
 
-	public static void main( String[] args ) throws InterruptedException , IOException {
+	public static void main( String[] args ) throws InterruptedException , IOException, IllegalArgumentException, PortInUseException {
+        CommPortIdentifier portID = TestVibe.getSerialCommPortIDbyName(SERIALPORTNAME);
+        if (portID == null) {
+            System.err.println("Found no available port "+ SERIALPORTNAME);
+            return;
+        }
+        
+        EchoServer.vibe = new Vibe(portID);
+        
 		int port = 1564;
 		try {
 			port = Integer.parseInt( args[ 0 ] );
