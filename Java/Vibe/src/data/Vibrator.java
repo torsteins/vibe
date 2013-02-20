392a8b6d62b29ed6948d4ae248b38f52a3d34b37
torsteins <torsteins@mi061170.lan>
2013-02-19 23:25:01 -0500

    Changing comments in Vibrator.java

--- Java/Vibe/src/data/Vibrator.java
+++ Java/Vibe/src/data/Vibrator.java
@@ -1,6 +1,18 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * The Vibe
+ *
+ * 18-549 Embedded Systems Design
+ * Spring 2013
+ * Group 6
+ *
+ * Debjani Biswas <dbiswas@cmu.edu>
+ * Xiao Bo Zhao <xiaoboz@andrew.cmu.edu>
+ * Jonathan Carreon <jcarreon@andrew.cmu.edu>
+ * Torstein Stromme <tstroemm@andrew.cmu.edu>
+ *
+ *
+ * Copyright (c) 2013 Carnegie Mellon University and/or its affiliates.
+ * All rights reserved.
  */
 package data;
 
