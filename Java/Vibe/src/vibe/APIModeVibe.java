6d904382ca3ff5fbf280672c3a146a469d05df64
torsteins <torsteins@mi061170.lan>
2013-02-27 01:17:56 -0500

    Made API mode import available

--- Java/Vibe/src/vibe/APIModeVibe.java
+++ Java/Vibe/src/vibe/APIModeVibe.java
@@ -16,10 +16,22 @@
  */
 package vibe;
 
+import com.rapplogic.xbee.api.XBee;
+import com.rapplogic.xbee.api.XBeeException;
+import org.apache.log4j.PropertyConfigurator;
+
 /**
  *
  * @author tstroemm
  */
 public class APIModeVibe {
-    
+    private static final String SERIALPORTNAME = "/dev/tty.usbserial-AD01UBSI";
+    private static final int BAUDRATE = 9600;
+
+    public static void main(String[] args) throws XBeeException {
+        PropertyConfigurator.configure("nbproject/log4j.properties");
+        XBee xbee = new XBee();
+        xbee.open(SERIALPORTNAME, BAUDRATE);
+        xbee.close();
+    }
 }
